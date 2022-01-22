6132cca1dd29a176edff8a96d7a1cb5b79f34362
fmpreferences <57734230+fmpreferences@users.noreply.github.com>
2022-01-22 17:19:08 -0500

    proper error handling for generate subs

--- reddits.py
+++ reddits.py
@@ -4,17 +4,11 @@ from argparse import ArgumentParser
 import re
 from bs4 import BeautifulSoup as bs
 import random
+import sys
 
 
 def main():
-    # argparse = ArgumentParser(description='Lorem Ipsum')
-    # argparse.add_argument('multi', type=str, help='the multi on your user')
-    # argparse.add_argument('reddits',
-    #                       type=str,
-    #                       help='reddits to analyze, separated by spaces')
-
-    # namespace = argparse.parse_args()
-    # argparse.add_argument('-s')
+    # namespace = reddits_parser()
     header_tags = 'h1', 'h2', 'h3', 'h4', 'h5'
     user_agent = 'script:substrial:v1.0 (by u/)'
     reddit = praw.Reddit(user_agent, user_agent=user_agent)
@@ -35,7 +29,7 @@ def main():
         for match in re.findall(r'r\/([A-Za-z0-9-_]+)', str(table))
     })
     print(subreddits)
-    print(generate_subreddits(subreddits, reddit, 10, (1, 10)))
+    print(generate_subreddits(subreddits, reddit, 10, 'content.w', (1, 10)))
     multireddit_name = ''  # namespace.multi
     try:
         print(
@@ -47,14 +41,33 @@ def main():
             reddit.multireddit.create(multireddit_name, [])
 
 
-def generate_subreddits(sub_pool, reddit, count: int, strata=None):
+def generate_subreddits(sub_pool, reddit, count: int, error=None, strata=None):
     def subscribers_least_to_greatest(sub):
         try:
             return reddit.subreddit(sub).subscribers
         except prawcore.Forbidden:
-            print(f'Forbidden access to r/{sub}; banned by reddit?')
+            errormsg = f'Access to r/{sub} is private'
+            if error is not None:
+                with open(error, 'a') as error_file:
+                    error_file.write(errormsg + '\n')
+            print(errormsg, file=sys.stderr)
             return 0
-        except prawcore.ResponseException:
+        except prawcore.Redirect:
+            errormsg = f'r/{sub} does not exist; possibly banned'
+            if error is not None:
+                with open(error, 'a') as error_file:
+                    error_file.write(errormsg + '\n')
+            print(errormsg, file=sys.stderr)
+            return 0
+        except prawcore.NotFound:
+            errormsg = f'r/{sub} has been banned'
+            if error is not None:
+                with open(error, 'a') as error_file:
+                    error_file.write(errormsg + '\n')
+            print(errormsg, file=sys.stderr)
+            return 0
+        except prawcore.ResponseException as e:
+            print(e, type(e), sub, file=sys.stderr)
             return 0
 
     subreddits = sorted(sub_pool,
@@ -68,6 +81,27 @@ def generate_subreddits(sub_pool, reddit, count: int, strata=None):
         subset = random.sample(subreddits, count)
     return subset
 
+
+# def reddits_parser():
+#     argparse = ArgumentParser(description='Lorem Ipsum')
+#     argparse.add_argument('multi', type=str, help='the multi on your user')
+#     argparse.add_argument('reddits',
+#                           type=str,
+#                           help='reddits to analyze, separated by spaces')
+#     argparse.add_argument('-r',
+#                           '--range',
+#                           type=str,
+#                           help='ranks of the subreddits to analyze')
+#     argparse.add_argument('-c',
+#                           '--count',
+#                           type=int,
+#                           help='number of subs to choose')
+#     argparse.add_argument('-e',
+#                           '--error',
+#                           type=str,
+#                           help='write errors to this file')
+
+#     return argparse.parse_args()
 
 if __name__ == '__main__':
     main()
